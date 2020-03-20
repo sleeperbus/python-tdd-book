aaf7d9598a7125568971789aa79182fe165d3116
hj <hj@toss.im>
2020-03-20 23:21:33 +0900

    * 입력을 위한 url 분리. home view의 기능을 위임함

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -9,7 +9,6 @@ MAX_WAIT = 10
 
 # TODO: Adjust model so that items are associated with different lists
 # TODO: Add unique URLs for each list
-# TODO: Add a URL for creating a new list via POST
 # TODO: Add URLs for adding a new item to an existing list via POST
 
 
