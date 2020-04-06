0ed3772af2de37a7c3a98ad869081167db85a846
hj <hj@toss.im>
2020-04-06 21:42:16 +0900

    * Use get_absolute_url on List model to DRY urls in views

--- superlists/urls.py
+++ superlists/urls.py
@@ -17,7 +17,6 @@ from django.conf.urls import url, include
 from lists import views as list_views
 from lists import urls as list_urls
 
-# TODO: Refactor away some duplication in urls.py
 urlpatterns = [
     url(r'^$', list_views.home_page, name='home'),
     url(r'^lists/', include(list_urls)),
