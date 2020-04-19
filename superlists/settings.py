dbca2c9f4301c9979da70a1adf36a4aa8cba5187
sleeperbus <sleeperbus@gmail.com>
2020-04-19 23:28:33 +0900

    * de-spiking email을 기반으로 한 사용자 모델을 만들고 인증을 위해 Token 모델을 사용함

--- superlists/settings.py
+++ superlists/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -105,6 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'accounts.User'
 
 
 # Internationalization
