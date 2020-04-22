d8a3386b14b06ca26de04c5debbe81ac7efb112d
hj <hj@toss.im>
2020-04-22 23:29:25 +0900

    * authentication 모듈 완성하고 settings에서 backend 대체

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -1,6 +1,19 @@
-
+from accounts.models import Token, User
 
 class PasswordlessAuthenticationBackend(object):
     def authenticate(self, uid):
-        pass
+        try:
+            token = Token.objects.get(uid=uid)
+            return User.objects.get(email=token.email)
+        except User.DoesNotExist:
+            return User.objects.create(email=token.email)
+        except Token.DoesNotExist:
+            return None
+
+    def get_user(self, email):
+        try:
+            return User.objects.get(email=email)
+        except User.DoesNotExist:
+            return None
+
 
